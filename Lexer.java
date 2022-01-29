efe28ffd4094d81a6a20efac7fe8c418163ff921
vcasteline <vcasteline@ufl.edu>
2022-01-29 16:47:38 -0500

    added classes to package ufl

--- Lexer.java
+++ Lexer.java
@@ -1,14 +1,16 @@
+package edu.ufl.cise.plc;
+
+
 import java.util.ArrayList;
 
-public class Lexer implements ILexer{
+public class Lexer implements ILexer {
     ArrayList<Token> tokens = new ArrayList<Token>();
 
     public Lexer(String input){
-        // for(int i=0; i<input.length(); i++){
-        //     tokens.add(new Token(input.charAt(i)));
-        // }
-        Token token = new Token();
-        token.kind = Kind.IDENT;
+        for(int i=0; i<input.length(); i++){
+            tokens.add(new Token(input.substring(0, 1)));
+        }
+        
     }
     public IToken next(){
 
--- Token.java
+++ Token.java
@@ -1,67 +1,104 @@
-class Token implements IToken{
+package edu.ufl.cise.plc;
+
+import edu.ufl.cise.plc.IToken;
+
+public class Token implements IToken {
     Kind kind;
     char token;
-
-    Token(){
-        switch(kind)
-        case IDENT:
-            kind = Kind.IDENT;
-        case INT_LIT:
-            kind = Kind.INT_LIT;
-        case FLOAT_LIT:
-            kind =  Kind.FLOAT_LIT;
-		case STRING_LIT:
-            kind = Kind.STRING_LIT;
-        case BOOLEAN_LIT:
-            kind = Kind.BOOLEAN_LIT;// 'true','false'
-		case LPAREN:
-            kind = Kind.LPAREN; // '('
-		case RPAREN:
-            kind = Kind.RPAREN; // ')'
-		case LSQUARE:
-            kind = Kind.LSQUARE; // '['
-		case RSQUARE:
-            kind = Kind.RSQUARE; // ']'
-		case LANGLE:
-            kind = Kind.LANGLE;  // '<<'
-		case RANGLE:
-            kind = Kind.RANGLE;  // '>>'
-		case PLUS:
-            kind = Kind.PLUS;  // '+'
-		case MINUS:
-            kind = Kind.MINUS; // '-'
-		case TIMES:
-            kind = Kind.TIMES;  // '*'
-		DIV,  //  '/'
-		MOD,  // '%'
-		COLOR_CONST, // 'BLACK','BLUE','CYAN','DARK_GRAY','GRAY','GREEN','LIGHT_GRAY','MAGENTA','ORANGE','PINK',
-					// 'RED','WHITE','YELLOW'
-		KW_IF, // 'if'
-		KW_FI, //'fi'
-		KW_ELSE, //'else'
-		KW_WRITE, // 'write'
-		KW_CONSOLE, // 'console'
-		AND, // '&'
-		OR,  // '|'
-		BANG, // '!'
-		LT,  // '<'
-		GT,  // '>'
-		EQUALS, // '=='
-		NOT_EQUALS, // '!='
-		LE, //  '<='
-		GE, //  '>='
-		TYPE, //int, float, string, boolean, color, image
-		COLOR_OP, //getRed, getGreen, getBlue
-		IMAGE_OP, //getWidth, getHeight
-		SEMI, // ';'
-		COMMA, // ','
-		ASSIGN, // '='
-		RARROW, // '->'
-		LARROW, // '<-'
-		KW_VOID, // 'void'
-		RETURN,// '^'
-		EOF, // used as sentinal, does not correspond to input
-		ERROR, // use to avoid exceptions if scanning all input at once
+    String input;
+    public Token(String input){
+        
+        switch(input){
+            // case "  ":
+            //     kind = Kind.IDENT;
+            // case INT_LIT:
+            //     kind = Kind.INT_LIT;
+            // case FLOAT_LIT:
+            //     kind =  Kind.FLOAT_LIT;
+            // case STRING_LIT:
+            //     kind = Kind.STRING_LIT;
+            case "true":
+                kind = Kind.BOOLEAN_LIT;// 'true','false'
+            case "false":
+                kind = Kind.BOOLEAN_LIT;
+            case "(":
+                kind = Kind.LPAREN; // '('
+            case ")":
+                kind = Kind.RPAREN; // ')'
+            case "[":
+                kind = Kind.LSQUARE; // '['
+            case "]":
+                kind = Kind.RSQUARE; // ']'
+            case "<<":
+                kind = Kind.LANGLE;  // '<<'
+            case ">>":
+                kind = Kind.RANGLE;  // '>>'
+            case "+":
+                kind = Kind.PLUS;  // '+'
+            case "-":
+                kind = Kind.MINUS; // '-'
+            case "*":
+                kind = Kind.TIMES;  // '*'
+            case "/":
+                kind = Kind.DIV;  //  '/'
+            case "%":
+                kind = Kind.MOD;  // '%'
+            // case "COLOR_CONST":
+            //     kind = Kind.COLOR_CONST; // 'BLACK','BLUE','CYAN','DARK_GRAY','GRAY','GREEN','LIGHT_GRAY','MAGENTA','ORANGE','PINK',
+            //             // 'RED','WHITE','YELLOW'
+            case "if":
+                kind = Kind.KW_IF; // 'if'
+            case "fi":
+                kind = Kind.KW_FI; //'fi'
+            case "else":
+                kind = Kind.KW_ELSE; //'else'
+            case "write":
+                kind = Kind.KW_WRITE; // 'write'
+            case "console":
+                kind = Kind.KW_CONSOLE; // 'console'
+            case "&":
+                kind = Kind.AND; // '&'
+            case "|":
+                kind = Kind.OR;  // '|'
+            case "!":
+                kind = Kind.BANG; // '!'
+            case "<":
+                kind = Kind.LT;  // '<'
+            case ">":
+                kind = Kind.GT;  // '>'
+            case "==":
+                kind = Kind.EQUALS; // '=='
+            case "!=":
+                kind = Kind.NOT_EQUALS; // '!='
+            case "<=":
+                kind = Kind.LE; //  '<='
+            case ">=":
+                kind = Kind.GE; //  '>='
+            // case "TYPE":
+            //     kind = Kind.TYPE; //int, float, string, boolean, color, image
+            // case COLOR_OP:
+            //     kind = Kind.COLOR_OP; //getRed, getGreen, getBlue
+            // case IMAGE_OP:
+            //     kind = Kind.IMAGE_OP; //getWidth, getHeight
+            case ";":
+                kind = Kind.SEMI; // ';'
+            case ",":
+                kind = Kind.COMMA; // ','
+            case "=":
+                kind = Kind.ASSIGN; // '='
+            case "->":
+                kind = Kind.RARROW; // '->'
+            case "<-":
+                kind = Kind.LARROW; // '<-'
+            case "void":
+                kind = Kind.KW_VOID; // 'void'
+            case "^":
+                kind = Kind.RETURN;// '^'
+            // case EOF:
+            //     kind = Kind.EOF; // used as sentinal, does not correspond to input
+            // case ERROR:
+            //     kind = Kind.ERROR; // use to avoid exceptions if scanning all input at once
+        }
     }
     //returns the token kind
 	public Kind getKind(){
@@ -75,7 +112,7 @@ class Token implements IToken{
 	//returns the characters in the source code that correspond to this token
 	//if the token is a STRING_LIT, this returns the raw characters, including delimiting "s and unhandled escape sequences.
 	public String getText(){
-
+        return input;
     };
 	
 	//returns the location in the source code of the first character of the token.
