26dc3f1e5d2257b8de586f0095e940b2a94e608f
vcasteline <vcasteline@ufl.edu>
2022-01-29 18:09:12 -0500

    first test passed

--- Lexer.java
+++ Lexer.java
@@ -1,21 +1,31 @@
 package edu.ufl.cise.plc;
 
-
+import java.io.*;
 import java.util.ArrayList;
 
 public class Lexer implements ILexer {
     ArrayList<Token> tokens = new ArrayList<Token>();
-
+    int currentToken = -1;
     public Lexer(String input){
-        for(int i=0; i<input.length(); i++){
-            tokens.add(new Token(input.substring(0, 1)));
+        String[] lines = input.split("\\r?\\n|\\r");
+        System.out.println(lines.length);
+        for(int i=0; i< lines.length; i++){
+            String line = lines[i];
+            String substring = line.substring(0, 1);
+//            if(substring.equals(" ")){
+//                break;
+//            }
+            tokens.add(new Token(substring, i, 0));
+
         }
-        
+        tokens.add(new Token("EOF", 0, 0));
     }
     public IToken next(){
+        currentToken++;
+        return tokens.get(currentToken);
 
     }
     public IToken peek(){
-
+        return tokens.get(0);
     }
 }
--- Token.java
+++ Token.java
@@ -6,7 +6,9 @@ public class Token implements IToken {
     Kind kind;
     char token;
     String input;
-    public Token(String input){
+    int line;
+    int col;
+    public Token(String input, int line, int col){
         
         switch(input){
             // case "  ":
@@ -19,61 +21,88 @@ public class Token implements IToken {
             //     kind = Kind.STRING_LIT;
             case "true":
                 kind = Kind.BOOLEAN_LIT;// 'true','false'
+                break;
             case "false":
                 kind = Kind.BOOLEAN_LIT;
+                break;
             case "(":
                 kind = Kind.LPAREN; // '('
+                break;
             case ")":
                 kind = Kind.RPAREN; // ')'
+                break;
             case "[":
                 kind = Kind.LSQUARE; // '['
+                break;
             case "]":
                 kind = Kind.RSQUARE; // ']'
+                break;
             case "<<":
                 kind = Kind.LANGLE;  // '<<'
+                break;
             case ">>":
                 kind = Kind.RANGLE;  // '>>'
+                break;
             case "+":
                 kind = Kind.PLUS;  // '+'
+                break;
             case "-":
                 kind = Kind.MINUS; // '-'
+                break;
             case "*":
                 kind = Kind.TIMES;  // '*'
+                break;
             case "/":
                 kind = Kind.DIV;  //  '/'
+                break;
             case "%":
                 kind = Kind.MOD;  // '%'
+                break;
             // case "COLOR_CONST":
             //     kind = Kind.COLOR_CONST; // 'BLACK','BLUE','CYAN','DARK_GRAY','GRAY','GREEN','LIGHT_GRAY','MAGENTA','ORANGE','PINK',
             //             // 'RED','WHITE','YELLOW'
             case "if":
                 kind = Kind.KW_IF; // 'if'
+                break;
             case "fi":
                 kind = Kind.KW_FI; //'fi'
+                break;
             case "else":
                 kind = Kind.KW_ELSE; //'else'
+                break;
             case "write":
                 kind = Kind.KW_WRITE; // 'write'
+                break;
             case "console":
                 kind = Kind.KW_CONSOLE; // 'console'
+                break;
             case "&":
                 kind = Kind.AND; // '&'
+                break;
             case "|":
                 kind = Kind.OR;  // '|'
+                break;
             case "!":
                 kind = Kind.BANG; // '!'
+                break;
             case "<":
                 kind = Kind.LT;  // '<'
+                break;
             case ">":
                 kind = Kind.GT;  // '>'
+                break;
             case "==":
                 kind = Kind.EQUALS; // '=='
+                break;
             case "!=":
                 kind = Kind.NOT_EQUALS; // '!='
+                break;
             case "<=":
                 kind = Kind.LE; //  '<='
+                break;
             case ">=":
                 kind = Kind.GE; //  '>='
+                break;
             // case "TYPE":
             //     kind = Kind.TYPE; //int, float, string, boolean, color, image
             // case COLOR_OP:
@@ -82,29 +111,40 @@ public class Token implements IToken {
             //     kind = Kind.IMAGE_OP; //getWidth, getHeight
             case ";":
                 kind = Kind.SEMI; // ';'
+                break;
             case ",":
                 kind = Kind.COMMA; // ','
+                break;
             case "=":
                 kind = Kind.ASSIGN; // '='
+                break;
             case "->":
                 kind = Kind.RARROW; // '->'
+                break;
             case "<-":
                 kind = Kind.LARROW; // '<-'
+                break;
             case "void":
                 kind = Kind.KW_VOID; // 'void'
+                break;
             case "^":
                 kind = Kind.RETURN;// '^'
-            // case EOF:
-            //     kind = Kind.EOF; // used as sentinal, does not correspond to input
+                break;
+            case "EOF":
+                 kind = Kind.EOF; // used as sentinal, does not correspond to input
             // case ERROR:
             //     kind = Kind.ERROR; // use to avoid exceptions if scanning all input at once
+
         }
+        this.line = line;
+        this.col = col;
+        System.out.println("input:"+ input);
+        System.out.println("line"+ line);
+        System.out.println("col"+ col);
     }
     //returns the token kind
 	public Kind getKind(){
-        Kind ourKind = Kind.IDENT;
-        
-        
+
         return kind;
 
     };
@@ -117,28 +157,28 @@ public class Token implements IToken {
 	
 	//returns the location in the source code of the first character of the token.
 	public SourceLocation getSourceLocation(){
-
+        return new SourceLocation(line,col);
     };
 
 	//returns the int value represented by the characters of this token if kind is INT_LIT
 	public int getIntValue(){
-
+        return 0;
     };
 
 	//returns the float value represented by the characters of this token if kind is FLOAT_LIT
 	public float getFloatValue(){
-
+        return 0;
     };
 
 	//returns the boolean value represented by the characters of this token if kind is BOOLEAN_LIT
 	public boolean getBooleanValue(){
-
+        return false;
     };
 	
 	//returns the String represented by the characters of this token if kind is STRING_LIT
 	//The delimiters should be removed and escape sequences replaced by the characters they represent.  
 	public String getStringValue(){
-
+        return "hello";
     };
 
 }
