7a06f4bdcb9c609996d97f3ed6512f6f7c65cff7
vcasteline <vcasteline@ufl.edu>
2022-01-29 20:04:12 -0500

    passed comment test

--- Lexer.java
+++ Lexer.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Lexer implements ILexer {
     ArrayList<Token> tokens = new ArrayList<Token>();
     int currentToken = -1;
+    int indentCheck = 0;
     public Lexer(String input){
         ArrayList<String> lines = new ArrayList<String>();
         Scanner scan =new Scanner(input);
@@ -26,25 +27,31 @@ public class Lexer implements ILexer {
 
                 char candidate = line.charAt(j);
                 boolean letterOrDigit = Character.isLetterOrDigit(candidate);
-                if (letterOrDigit == false && candidate != ' ' && candidate != '\"') {
+                if (letterOrDigit == false && candidate != ' ' && candidate != '\"'&& candidate != '#') {
                     if(stringInput.isEmpty())
                     {
-                    tokens.add(new Token(String.valueOf(candidate), i, j)); //Add token if input is not a number/letter
+                    tokens.add(new Token(String.valueOf(candidate), i, 0)); //Add token if input is not a number/letter
                     }
                     else {
-                        tokens.add(new Token(stringInput, i, j)); //Send stringInput as a token
+                        tokens.add(new Token(stringInput, i, 0)); //Send stringInput as a token
                         stringInput = ""; //Flush string input
-                        tokens.add(new Token(String.valueOf(candidate), i, j)); //send non letter/number as a token
+                        tokens.add(new Token(String.valueOf(candidate), i, 0)); //send non letter/number as a token
                     }
 
                 }
                 else {
                     stringInput = stringInput + candidate;
-                    if(j == lineSize-1)
-                    {
-                        tokens.add(new Token(stringInput, i, j)); //If we get to the end of line, send stringInput as a token
-                        stringInput = "";
-                    }
+                    //if(candidate!=' '){
+                        if(j == lineSize-1)
+                        {
+                            if (stringInput.charAt(0) != '#') {
+                                tokens.add(new Token(stringInput, i, 0)); //If we get to the end of line, send stringInput as a token
+                            }
+                            stringInput="";
+
+                        }
+                    //}
+
                 }
 
             }
--- Token.java
+++ Token.java
@@ -9,132 +9,137 @@ public class Token implements IToken {
     int line;
     int col;
     public Token(String input, int line, int col){
-        
-        switch(input){
-            // case "  ":
-            //     kind = Kind.IDENT;
-            // case INT_LIT:
-            //     kind = Kind.INT_LIT;
-            // case FLOAT_LIT:
-            //     kind =  Kind.FLOAT_LIT;
-            // case STRING_LIT:
-            //     kind = Kind.STRING_LIT;
-            case "true":
-                kind = Kind.BOOLEAN_LIT;// 'true','false'
-                break;
-            case "false":
-                kind = Kind.BOOLEAN_LIT;
-                break;
-            case "(":
-                kind = Kind.LPAREN; // '('
-                break;
-            case ")":
-                kind = Kind.RPAREN; // ')'
-                break;
-            case "[":
-                kind = Kind.LSQUARE; // '['
-                break;
-            case "]":
-                kind = Kind.RSQUARE; // ']'
-                break;
-            case "<<":
-                kind = Kind.LANGLE;  // '<<'
-                break;
-            case ">>":
-                kind = Kind.RANGLE;  // '>>'
-                break;
-            case "+":
-                kind = Kind.PLUS;  // '+'
-                break;
-            case "-":
-                kind = Kind.MINUS; // '-'
-                break;
-            case "*":
-                kind = Kind.TIMES;  // '*'
-                break;
-            case "/":
-                kind = Kind.DIV;  //  '/'
-                break;
-            case "%":
-                kind = Kind.MOD;  // '%'
-                break;
-            // case "COLOR_CONST":
-            //     kind = Kind.COLOR_CONST; // 'BLACK','BLUE','CYAN','DARK_GRAY','GRAY','GREEN','LIGHT_GRAY','MAGENTA','ORANGE','PINK',
-            //             // 'RED','WHITE','YELLOW'
-            case "if":
-                kind = Kind.KW_IF; // 'if'
-                break;
-            case "fi":
-                kind = Kind.KW_FI; //'fi'
-                break;
-            case "else":
-                kind = Kind.KW_ELSE; //'else'
-                break;
-            case "write":
-                kind = Kind.KW_WRITE; // 'write'
-                break;
-            case "console":
-                kind = Kind.KW_CONSOLE; // 'console'
-                break;
-            case "&":
-                kind = Kind.AND; // '&'
-                break;
-            case "|":
-                kind = Kind.OR;  // '|'
-                break;
-            case "!":
-                kind = Kind.BANG; // '!'
-                break;
-            case "<":
-                kind = Kind.LT;  // '<'
-                break;
-            case ">":
-                kind = Kind.GT;  // '>'
-                break;
-            case "==":
-                kind = Kind.EQUALS; // '=='
-                break;
-            case "!=":
-                kind = Kind.NOT_EQUALS; // '!='
-                break;
-            case "<=":
-                kind = Kind.LE; //  '<='
-                break;
-            case ">=":
-                kind = Kind.GE; //  '>='
-                break;
-            // case "TYPE":
-            //     kind = Kind.TYPE; //int, float, string, boolean, color, image
-            // case COLOR_OP:
-            //     kind = Kind.COLOR_OP; //getRed, getGreen, getBlue
-            // case IMAGE_OP:
-            //     kind = Kind.IMAGE_OP; //getWidth, getHeight
-            case ";":
-                kind = Kind.SEMI; // ';'
-                break;
-            case ",":
-                kind = Kind.COMMA; // ','
-                break;
-            case "=":
-                kind = Kind.ASSIGN; // '='
-                break;
-            case "->":
-                kind = Kind.RARROW; // '->'
-                break;
-            case "<-":
-                kind = Kind.LARROW; // '<-'
-                break;
-            case "void":
-                kind = Kind.KW_VOID; // 'void'
-                break;
-            case "^":
-                kind = Kind.RETURN;// '^'
-                break;
-            case "EOF":
-                 kind = Kind.EOF; // used as sentinal, does not correspond to input
-            // case ERROR:
-            //     kind = Kind.ERROR; // use to avoid exceptions if scanning all input at once
+        if(input.charAt(0) == '\"'){
+            kind = Kind.STRING_LIT;
+        }
+        else {
+            switch (input) {
+                 case "    ":
+                     kind = Kind.IDENT;
+                // case INT_LIT:
+                //     kind = Kind.INT_LIT;
+                // case FLOAT_LIT:
+                //     kind =  Kind.FLOAT_LIT;
+                // case STRING_LIT:
+                //     kind = Kind.STRING_LIT;
+                case "true":
+                    kind = Kind.BOOLEAN_LIT;// 'true','false'
+                    break;
+                case "false":
+                    kind = Kind.BOOLEAN_LIT;
+                    break;
+                case "(":
+                    kind = Kind.LPAREN; // '('
+                    break;
+                case ")":
+                    kind = Kind.RPAREN; // ')'
+                    break;
+                case "[":
+                    kind = Kind.LSQUARE; // '['
+                    break;
+                case "]":
+                    kind = Kind.RSQUARE; // ']'
+                    break;
+                case "<<":
+                    kind = Kind.LANGLE;  // '<<'
+                    break;
+                case ">>":
+                    kind = Kind.RANGLE;  // '>>'
+                    break;
+                case "+":
+                    kind = Kind.PLUS;  // '+'
+                    break;
+                case "-":
+                    kind = Kind.MINUS; // '-'
+                    break;
+                case "*":
+                    kind = Kind.TIMES;  // '*'
+                    break;
+                case "/":
+                    kind = Kind.DIV;  //  '/'
+                    break;
+                case "%":
+                    kind = Kind.MOD;  // '%'
+                    break;
+                // case "COLOR_CONST":
+                //     kind = Kind.COLOR_CONST; // 'BLACK','BLUE','CYAN','DARK_GRAY','GRAY','GREEN','LIGHT_GRAY','MAGENTA','ORANGE','PINK',
+                //             // 'RED','WHITE','YELLOW'
+                case "if":
+                    kind = Kind.KW_IF; // 'if'
+                    break;
+                case "fi":
+                    kind = Kind.KW_FI; //'fi'
+                    break;
+                case "else":
+                    kind = Kind.KW_ELSE; //'else'
+                    break;
+                case "write":
+                    kind = Kind.KW_WRITE; // 'write'
+                    break;
+                case "console":
+                    kind = Kind.KW_CONSOLE; // 'console'
+                    break;
+                case "&":
+                    kind = Kind.AND; // '&'
+                    break;
+                case "|":
+                    kind = Kind.OR;  // '|'
+                    break;
+                case "!":
+                    kind = Kind.BANG; // '!'
+                    break;
+                case "<":
+                    kind = Kind.LT;  // '<'
+                    break;
+                case ">":
+                    kind = Kind.GT;  // '>'
+                    break;
+                case "==":
+                    kind = Kind.EQUALS; // '=='
+                    break;
+                case "!=":
+                    kind = Kind.NOT_EQUALS; // '!='
+                    break;
+                case "<=":
+                    kind = Kind.LE; //  '<='
+                    break;
+                case ">=":
+                    kind = Kind.GE; //  '>='
+                    break;
+                // case "TYPE":
+                //     kind = Kind.TYPE; //int, float, string, boolean, color, image
+                // case COLOR_OP:
+                //     kind = Kind.COLOR_OP; //getRed, getGreen, getBlue
+                // case IMAGE_OP:
+                //     kind = Kind.IMAGE_OP; //getWidth, getHeight
+                case ";":
+                    kind = Kind.SEMI; // ';'
+                    break;
+                case ",":
+                    kind = Kind.COMMA; // ','
+                    break;
+                case "=":
+                    kind = Kind.ASSIGN; // '='
+                    break;
+                case "->":
+                    kind = Kind.RARROW; // '->'
+                    break;
+                case "<-":
+                    kind = Kind.LARROW; // '<-'
+                    break;
+                case "void":
+                    kind = Kind.KW_VOID; // 'void'
+                    break;
+                case "^":
+                    kind = Kind.RETURN;// '^'
+                    break;
+                case "EOF":
+                    kind = Kind.EOF; // used as sentinal, does not correspond to input
+                    break;
+                default:
+                    kind = Kind.ERROR; // use to avoid exceptions if scanning all input at once
 
+            }
         }
         this.line = line;
         this.col = col;
